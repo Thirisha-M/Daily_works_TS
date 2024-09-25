a4ef0e24ec12f59c02e16fab4dbe3752034d39ad
Thirisha-M <thirisha.m20@gmail.com>
2024-09-25 10:03:19 +0530

    updated file

---
+++ Day7/loops.js
@@ -0,0 +1,14 @@
+//for loop:
+//for loop
+//for..of loop
+//for..in loop
+//while
+//do-while
+//1 to 10
+for (var i = 1; i < 10; i++) {
+    console.log(i);
+}
+//0 2 4 6 8 10
+for (var i = 0; i <= 10; i = i + 2) {
+    console.log(i);
+}
--- Day7/loops.ts
+++ Day7/loops.ts
@@ -5,6 +5,11 @@
 //while
 //do-while
 
+//1 to 10
 for(let i=1;i<10;i++){
+    console.log(i);
+}
+//0 2 4 6 8 10
+for(let i=0;i<=10;i=i+2){
     console.log(i);
 }
