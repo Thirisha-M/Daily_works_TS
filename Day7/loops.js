5e85094019b85e739c23f6f232fe86cdfc50bea0
Thirisha-M <thirisha.m20@gmail.com>
2024-09-26 10:02:28 +0530

    updated file

--- Day7/loops.js
+++ Day7/loops.js
@@ -2,8 +2,6 @@
 //for loop
 //for..of loop
 //for..in loop
-//while
-//do-while
 //1 to 10
 for (var i = 1; i < 10; i++) {
     console.log(i);
@@ -31,6 +29,62 @@ for (var i in num) {
 }
 var info = "Hello World";
 for (var _a = 0, info_1 = info; _a < info_1.length; _a++) {
-    var c = info_1[_a];
-    console.log(c);
+    var c_1 = info_1[_a];
+    console.log(c_1);
 }
+//whileloop
+// 1 to 10
+var p = 1;
+while (p <= 10) {
+    console.log(p);
+    p++;
+}
+//use case for while:when number of iteration are not fixed:
+//1.infinite scrolling:zomato,swiggy,linkedin page..
+//2.wait for page loading:2,5,10,15 secs
+//3.wait for the element on the page;2,3,5,10 secs
+//4.calender handling
+//5.webtable pagination:pre...1 2 3 4 5 6....next
+//use cases for for loop:
+//when number of interatrions are fxed:
+// drop down:month:jan to dec:1 to 12
+//values are available and array list
+console.log("-----------");
+//do while:
+var d = 1;
+do {
+    console.log(d); //123...10
+    d++; //23
+} while (d <= 10);
+console.log("------------");
+var c = 1;
+do {
+    console.log(c); //1
+    d++;
+} while (c >= 10); //false
+console.log("------------");
+//continue 
+for (var index_1 = 0; index_1 < 9; index_1++) {
+    if (index_1 % 2)
+        continue;
+    console.log(index_1);
+}
+console.log("------------");
+//continue in while loop
+var index = -1;
+while (index < 9) {
+    index = index + 1;
+    if (index % 2)
+        continue;
+    console.log(index);
+}
+//continue in Do while
+var indexs = 9;
+var count = 0;
+do {
+    indexs += 1;
+    if (indexs % 2)
+        continue;
+    count += 1;
+} while (indexs < 99);
+console.log(count); // 45
--- Day7/loops.ts
+++ Day7/loops.ts
@@ -2,8 +2,6 @@
     //for loop
     //for..of loop
     //for..in loop
-//while
-//do-while
 
 //1 to 10
 for(let i=1;i<10;i++){
@@ -37,4 +35,130 @@ for(let c of info ){
 }
 
 //whileloop
+// 1 to 10
+
+let p=1;
+while(p<=10){
+    console.log(p);
+    p++;
+}
+
+//use case for while:when number of iteration are not fixed:
+//1.infinite scrolling:zomato,swiggy,linkedin page..
+//2.wait for page loading:2,5,10,15 secs
+//3.wait for the element on the page;2,3,5,10 secs
+//4.calender handling
+//5.webtable pagination:pre...1 2 3 4 5 6....next
+
+
+//use cases for for loop:
+//when number of interatrions are fxed:
+// drop down:month:jan to dec:1 to 12
+//values are available and array list
+console.log("-----------");
+//do while:
+
+let d=1;
+do{
+    console.log(d);//123...10
+    d++;//23
+}
+while(d<=10);
+
+console.log("------------");
+
+let c=1;
+do{
+    console.log(c);//1
+    d++;
+}
+while(c>=10);//false
+
+console.log("------------");
+
+//continue 
+for (let index = 0; index < 9; index++) {
+    if (index % 2)
+        continue;
+    console.log(index);
+}
+
+console.log("------------");
+
+//continue in while loop
+
+let index = -1;
+
+while (index < 9) {
+
+    index = index + 1;
+
+    if (index % 2)
+        continue;
+
+    console.log(index);
+}
+
+console.log("------------");
+
+//continue in Do while
+
+let indexs = 9;
+let count = 0;
+do {
+    indexs+= 1;
+    if (indexs % 2)
+        continue;
+    count += 1;
+} while (indexs < 99);
+console.log(count); // 45
+
+//break in for loop
+
+let products = [
+    { name: 'phone', price: 700 },
+    { name: 'tablet', price: 900 },
+    { name: 'laptop', price: 1200 }
+];
+
+for (var i = 0; i < products.length; i++) {
+    if (products[i].price == 900)
+        break;
+}
+console.log(products[i]);
+
+// break in switchcase
+let products1 = [
+    { name: 'phone', price: 700 },
+    { name: 'tablet', price: 900 },
+    { name: 'laptop', price: 1200 }
+];
+
+let discount = 0;
+let product = products1[1];
+
+switch (product.name) {
+    case 'phone':
+        discount = 5;
+        break;
+    case 'tablet':
+        discount = 10;
+        break;
+    case 'laptop':
+        discount = 15;
+        break;
+}
+
+console.log(`There is a ${discount}% on ${product.name}.`);
+
+/* break in while loop
+    while(condition) {
+     do something
+     ...
+
+    if(anotherCondition) 
+         break;
+ }*/
+
+
 
